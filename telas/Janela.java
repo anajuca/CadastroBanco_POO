c52951148f74dac07242380d79b050a100edb189
Ana Júlia <anaju.aoliveiraa@gmail.com>
2025-08-23 16:52:09 -0300

    fazer desafios

--- telas/Janela.java
+++ telas/Janela.java
@@ -11,90 +11,125 @@ import javax.swing.JSeparator;
 import funcoes.metodos;
 
 public class Janela extends JFrame {
-    public static void main(String[] args){
-        Janela janela = new Janela();
 
-        JFrame frame = new JFrame("Java Swing - Desenvolvimento de Sistemas");
+    metodos funcoes = new metodos();
+    JLabel jlAgencia = new JLabel();
+    JTextField jtfAgencia = new JTextField();
+    JLabel jlConta = new JLabel();
+    JTextField jtfConta = new JTextField();
+    JSeparator jSeparator01 = new JSeparator();
+    JLabel jlNome = new JLabel("Nome:");
+    JTextField jtfNome = new JTextField();
+    JLabel jlEndereco = new JLabel("Endereço:");
+    JTextField jtfEndereco = new JTextField();
+    JLabel jlTelefone = new JLabel("Telefone");
+    JTextField jtfTelefone = new JTextField();
+    JLabel jlCpf = new JLabel("CPF.");
+    JTextField jtfCpf = new JTextField();
+    JRadioButton jrbCorrente = new JRadioButton("Conta Corrente");
+    JRadioButton jrbPoupanca = new JRadioButton("Conta Poupança");
+    ButtonGroup bgContas = new ButtonGroup();
+    JSeparator jSeparator02 = new JSeparator();
+    JButton jbConsultar = new JButton("Consultar");
+    JButton jbAtualizar = new JButton("Atualizar");
+    JButton jbFechar = new JButton("Fechar");
 
-        frame.setSize(400, 255);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.getContentPane().setLayout(null);
+    public Janela(){
 
-        metodos funcoes = new metodos();
-        funcoes.centralizar(frame);
-
-        JLabel jlAgencia = new JLabel();
+        setSize(400, 255);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        getContentPane().setLayout(null);
+        funcoes.centralizar(this);
+        
         jlAgencia.setText("Código da Agência");
         jlAgencia.setBounds(10, 10, 110, 18);
-        JTextField jtfAgencia = new JTextField(20);
-        jtfAgencia.setBounds(125, 10, 50, 20);
-        frame.getContentPane().add(jlAgencia);
-        frame.getContentPane().add(jtfAgencia);
+        getContentPane().add(jlAgencia);
 
-        JLabel jlConta = new JLabel();
+        jtfAgencia.setBounds(125, 10, 50, 20);
+        getContentPane().add(jtfAgencia);
+
+        
         jlConta.setText("Número da Conta");
         jlConta.setBounds(205, 10, 105, 18);
-        JTextField jtfConta = new JTextField(20);
+        getContentPane().add(jlConta);
+
         jtfConta.setBounds(315, 10, 60, 20);
-        frame.getContentPane().add(jlConta);
-        frame.getContentPane().add(jtfConta);
+        getContentPane().add(jtfConta);
 
-        JSeparator jSeparator01 = new JSeparator();
+        
         jSeparator01.setBounds(10, 40, 365, 10);
-        frame.getContentPane().add(jSeparator01);
+        getContentPane().add(jSeparator01);
 
-        JLabel jlNome = new JLabel("Nome:");
+        
         jlNome.setBounds(10, 50, 60, 18);
         jlNome.setHorizontalAlignment(SwingConstants.RIGHT);
-        JTextField jtfNome = new JTextField(20);
-        jtfNome.setBounds(75, 50, 300, 20);
-        frame.getContentPane().add(jlNome);
-        frame.getContentPane().add(jtfNome);
+        getContentPane().add(jlNome);
 
-        JLabel jlEndereco = new JLabel("Endereço:");
+        jtfNome.setBounds(75, 50, 300, 20);
+        getContentPane().add(jtfNome);
+        
         jlEndereco.setBounds(10, 75, 60, 18);
         jlEndereco.setHorizontalAlignment(SwingConstants.RIGHT);
-        JTextField jtfEndereco = new JTextField(20);
-        jtfEndereco.setBounds(75, 75, 300, 20);
-        frame.getContentPane().add(jlEndereco);
-        frame.getContentPane().add(jtfEndereco);
+        getContentPane().add(jlEndereco);
 
-        JLabel jlTelefone = new JLabel("Telefone");
+        jtfEndereco.setBounds(75, 75, 300, 20); 
+        getContentPane().add(jtfEndereco);
+
+        
         jlTelefone.setBounds(10, 100, 60, 18);
         jlTelefone.setHorizontalAlignment(SwingConstants.RIGHT);
-        JTextField jtfTelefone = new JTextField(20);
-        jtfTelefone.setBounds(75, 100, 300, 20);
-        frame.getContentPane().add(jlTelefone);
-        frame.getContentPane().add(jtfTelefone);
+        getContentPane().add(jlTelefone);
 
-        JLabel jlCpf = new JLabel("CPF.");
+        jtfTelefone.setBounds(75, 100, 300, 20);   
+        getContentPane().add(jtfTelefone);
+
+        
         jlCpf.setBounds(10, 125, 60, 18);
-        JTextField jtfCpf = new JTextField(20);
+        getContentPane().add(jlCpf);
+
         jtfCpf.setBounds(100, 150, 111, 20);
-        frame.getContentPane().add(jlCpf);
-        frame.getContentPane().add(jtfCpf);
+        getContentPane().add(jtfCpf);
 
-        JRadioButton jrbCorrente = new JRadioButton("Conta Corrente");
-        JRadioButton jrbPoupanca = new JRadioButton("Conta Poupança");
+        
+        jrbCorrente.setBounds(100, 150, 111, 20);
+        jrbCorrente.setMnemonic('C');
+        jrbCorrente.setSelected(true);
+        getContentPane().add(jrbCorrente);
 
-        ButtonGroup bgContas = new ButtonGroup();
+        jrbPoupanca.setBounds(225, 150, 118, 20);
+        jrbPoupanca.setMnemonic('P');
+        getContentPane().add(jrbPoupanca);
+
+        
         bgContas.add(jrbCorrente);
         bgContas.add(jrbPoupanca);
 
-        JSeparator jSeparator02 = new JSeparator();
-        frame.add(jSeparator02);
+        
+        jSeparator02.setBounds(10, 180, 365, 10);
+        getContentPane().add(jSeparator02);
 
-        JButton jbConsultar = new JButton("Consultar");
-        JButton jbAtualizar = new JButton("Atualizar");
-        JButton jbFechar = new JButton("Fechar");
-        frame.add(jbConsultar);
-        frame.add(jbAtualizar);
-        frame.add(jbFechar);
+        
+        jbConsultar.setBounds(35, 190, 100, 23);
+        jbConsultar.setMnemonic('S');
+        getContentPane().add(jbConsultar);
 
+        jbAtualizar.setBounds(145, 190, 100, 23);
+        jbAtualizar.setMnemonic('A');
+        jbAtualizar.setEnabled(false);
+        getContentPane().add(jbAtualizar);
+
+        jbFechar.setBounds(225, 190, 100, 23);
+        jbFechar.setMnemonic('F');
+        getContentPane().add(jbFechar);
+
+    }
+   
+    public static void main(String[] args){
+
+        Janela janela = new Janela();
         janela.setVisible(true);
-
 
     }
 }
